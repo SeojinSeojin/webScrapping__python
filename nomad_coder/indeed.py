e0a523d18c0bd76313e771afbfe07bf519ac2731
Seojin Kim <1106laura@naver.com>
2020-03-08 01:55:18 +0900

    2020-03-07 : 2.5~2.8 - indeed pages

---
+++ nomad_coder/indeed.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 20
+INDEED_URL = "https://www.indeed.jobs/career/SearchJobs/python"
+
+def extract_indeed_pages():
+    result = requests.get(INDEED_URL)
+
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class":"listPagination"})
+    links = pagination.find_all('a')
+
+    pages=[]
+    for link in links[:-1] :
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+
+    return max_page
+
+def extract_indeed_jobs(last_page) :
+    jobs=[]
+    locs=[]
+    for page in range(last_page) : 
+        result = requests.get(f"{INDEED_URL}?jobOffset={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("h3", {"class":"listSingleColumnItemTitle"})
+        for result in results :
+            job = result.find("a").string.split("-")[0]
+            jobs.append(job)
+    return jobs
+
+# 직업 이름, 지역만 추출 가능하다. . indeed 사이트가 바뀌었나보다 ㅜㅜ
--- nomad_coder/main.py
+++ nomad_coder/main.py
@@ -1,15 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.jobs/career/SearchJobs/python")
+last_indeed_page = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-pagination = indeed_soup.find("div", {"class":"listPagination"})
-links = pagination.find_all('a')
+print(extract_indeed_jobs(last_indeed_page))
 
-pages=[]
-for link in links[:-1] :
-    pages.append(int(link.string))
-max_page = pages[-1]
-
-print(pages)
