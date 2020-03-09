8b38208d6858616a666a402ad9013ebd70360709
Seojin Kim <1106laura@naver.com>
2020-03-10 01:45:38 +0900

    2020-03-10 : Stack overclow CSV save completed!

--- nomad_coder/so.py
+++ nomad_coder/so.py
@@ -1,29 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+from save import save_to_file
 
 LIMIT = 50
 URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 
-def get_last_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")  
     pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
     last_page = pages[-2].get_text(strip=True)
     return int(last_page)
 
+def extract_job(html):
+    title = html.find("a", {"class":"s-link stretched-link"})["title"]
+    company = html.find("h3", {"class":"fc-black-700"}).find("span").get_text(strip=True)
+    location = html.find("h3", {"class":"fc-black-700"}).find("span", {"class":"fc-black-500"}).get_text(strip="\r")
+    job_id = html["data-jobid"]
+    return {"title": title, "company": company, "location": location, "apply_link":f"https://stackoverflow.com/jobs/{job_id}"}
+
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"extracting {page+1} page now")
         result=requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
-        results = soup.find_all("a", {"class":"s-link"})
+        results = soup.find_all("div", {"class":"-job"})
         for result in results :
             job = extract_job(result)
+            print(job)
             jobs.append(job)
     return jobs
 
 def get_jobs() :
     last_page = get_last_page()
     jobs = extract_jobs(last_page)
+    return jobs
 
-extract_jobs(166)
+jobs = get_jobs()
+save_to_file(jobs)
